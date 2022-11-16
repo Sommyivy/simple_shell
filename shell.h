be7ccc3e61f71e7b8a1811c9b46bf3955c1ad27a
Unigwe Somtochukwu <somtochukwu.unigwe.203748@unn.edu.ng>
2022-11-16 09:44:24 -0800

    all done

--- shell.h
+++ shell.h
@@ -1,10 +1,20 @@
 #ifndef SHELL_H
 #define SHELL_H
 
+void print_prompt1(void);
+void print_prompt2(void);
+
+char *read_cmd(void);
+
 #include <stdio.h>
+#include <unistd.h>
 #include <stdlib.h>
+#include <sys/types.h>
+#include <sys/wait.h>
+#include <sys/stat.h>
 #include <errno.h>
-#include <string.h>
+#include <fcntl.h>
+#include <signal.h>
+#include <limits.h>
 
-
- #endif
+#endif
